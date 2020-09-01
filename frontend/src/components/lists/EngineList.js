4abcd3017007a8c6f391a1f6b99b010de83299a5
victoradt <audetat.v@gmail.com>
2020-09-01 22:05:25 +0200

    [AND WE HAVE A LIFTOFF]

--- frontend/src/components/lists/EngineList.js
+++ frontend/src/components/lists/EngineList.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import Button from 'react-bootstrap/Button'
+import Vulcain from './../../assets/image/engines/Vulcain.svg'
+import F1 from './../../assets/image/engines/F-1.svg'
+import Merlin from './../../assets/image/engines/Merlin.svg'
+import RD180 from './../../assets/image/engines/RD-180.svg'
+import RS25 from './../../assets/image/engines/RS-25.svg'
+
+
 
 export default class EngineList extends Component {
     state = {
@@ -35,7 +42,21 @@ export default class EngineList extends Component {
                                 <p> Thrust : {e.thrust} kn</p>
                                 <Button className="button__add__part" variant="light" onClick={() => this.props.addPart(e, e.partType)} >Add part</Button>
                             </div>
-                            <img alt={`${e.name} pictogram`} src={`http://localhost:1337${e.thumb.url}`} />
+                            {e.name === 'RS-25' &&
+                                <img alt={`${e.name} pictogram`} src={RS25} />
+                            }
+                            {e.name === 'F-1' &&
+                                <img alt={`${e.name} pictogram`} src={F1} />
+                            }
+                            {e.name === 'Merlin' &&
+                                <img alt={`${e.name} pictogram`} src={Merlin} />
+                            }
+                            {e.name === 'RD-180' &&
+                                <img alt={`${e.name} pictogram`} src={RD180} />
+                            }
+                            {e.name === 'Vulcain' &&
+                                <img alt={`${e.name} pictogram`} src={Vulcain} />
+                            }
                         </div>
                     ))}
                 </div>
